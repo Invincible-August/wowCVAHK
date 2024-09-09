a1e126d26ad53246ca3112026e0d8924d164a781
Invincible-August <m15085157874@163.com>
2024-09-09 23:55:28 +0800

    1

--- ButtonControl.cs
+++ ButtonControl.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
 using System.Text;
+using System.Windows.Forms;
 
 namespace wowCVAHK
 {
@@ -42,9 +43,11 @@ namespace wowCVAHK
 
         internal static void running()
         {
+            Console.WriteLine("开始调试");
             // 加载配置文件
             IniFile ini = new IniFile(Constant.INI_FILE_PATH);
-            string windowId = ini.Get("windowId");  // 获取窗口 ID
+            string windowId = ini.Get("windowHandle");  // 获取窗口 ID
+            Console.WriteLine($"windowId = {windowId}");
             string[] locationParts = ini.Get("coordinate").Split(',');  // 获取坐标
             int coordX = int.Parse(locationParts[0]);
             int coordY = int.Parse(locationParts[1]);
@@ -53,47 +56,57 @@ namespace wowCVAHK
 
             // 绑定窗口
             // 查找指定窗口
-            IntPtr hWnd = BaseAPI.FindWindow(null, windowId);  // 假设 ini 中 windowId 是窗口标题
-            if (hWnd == IntPtr.Zero)
+            if (int.TryParse(windowId, out int intWindowId)) 
             {
-                Console.WriteLine("找不到指定窗口。");
-                return;
-            }
-            IntPtr foregroundWindow = BaseAPI.GetForegroundWindow();
-            if (hWnd == foregroundWindow)
-            {
-                Console.WriteLine("该窗口在最前面");
+                IntPtr hWnd = new IntPtr(intWindowId);  // 假设 ini 中 windowId 是窗口标题
+                if (hWnd == IntPtr.Zero)
+                {
+                    Console.WriteLine("找不到指定窗口。");
+                    return;
+                }
+                IntPtr foregroundWindow = BaseAPI.GetForegroundWindow();
+
+                Console.WriteLine($"hWnd ={hWnd}， foregroundWindow={foregroundWindow}");
+                if (hWnd == foregroundWindow)
+                {
+                    Console.WriteLine("该窗口在最前面");
+                }
+                else
+                {
+                    bool showWindowResult = BaseAPI.ShowWindow(hWnd, Constant.SW_SHOWMAXIMIZED);
+                    bool result = BaseAPI.SetForegroundWindow(hWnd);
+                }
+
+
+                //获取窗口坐标
+                BaseAPI.GetWindowRect(hWnd, out BaseAPI.RECT rect);
+                Console.WriteLine($"rect.Left={rect.Left},rect.Top={rect.Top},rect.Right - rect.Left={rect.Right - rect.Left},rect.Bottom - rect.Top={rect.Bottom - rect.Top}");
+
+                //捕获窗口的屏幕截图并转换为 Mat 格式
+                Bitmap screenshot = Utils.CaptureWindow(rect.Left, rect.Top, rect.Right - rect.Left, rect.Bottom - rect.Top);
+                Mat matImage = new Mat();
+                Utils.BitmapToMat(screenshot, matImage);
+
+                // 将 Mat 转换为 EmguCV 的 Image<Bgr, Byte>
+                //Image<Bgr, Byte> img = matImage.ToImage<Bgr, Byte>();
+
+                //// *************************************************************************
+                //// TODO 获取到的图片先进行测试，测试代码，测试通过后需要删除
+                //Bitmap bitmap = img.ToBitmap();
+                //string filePath = "screenshot.jpg";
+                //bitmap.Save(filePath);
+                //Process.Start(new ProcessStartInfo(filePath) { UseShellExecute = true });
+                // *************************************************************************
+
+
+                // 进行色块判断
+                // 下发输入指令
             }
             else
             {
-                bool showWindowResult = BaseAPI.ShowWindow(hWnd, Constant.SW_SHOWMAXIMIZED);
-                bool result = BaseAPI.SetForegroundWindow(hWnd);
+                MessageBox.Show("窗口未绑定", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
-
-
-            // 获取窗口坐标
-            BaseAPI.GetWindowRect(hWnd, out BaseAPI.RECT rect);
-            Console.WriteLine($"rect.Left={rect.Left},rect.Top={rect.Top},rect.Right - rect.Left={rect.Right - rect.Left},rect.Bottom - rect.Top={rect.Bottom - rect.Top}");
-
-            // 捕获窗口的屏幕截图并转换为 Mat 格式
-            Bitmap screenshot = Utils.CaptureWindow(rect.Left, rect.Top, rect.Right - rect.Left, rect.Bottom - rect.Top);
-            Mat matImage = new Mat();
-            Utils.BitmapToMat(screenshot, matImage);
-
-            // 将 Mat 转换为 EmguCV 的 Image<Bgr, Byte>
-            Image<Bgr, Byte> img = matImage.ToImage<Bgr, Byte>();
-
-            // *************************************************************************
-            // TODO 获取到的图片先进行测试，测试代码，测试通过后需要删除
-            Bitmap bitmap = img.ToBitmap();
-            string filePath = "screenshot.jpg";
-            bitmap.Save(filePath);
-            Process.Start(new ProcessStartInfo(filePath) { UseShellExecute = true });
-            // *************************************************************************
-
-
-            // 进行色块判断
-            // 下发输入指令
+            
         }
     }
 }
--- utils.cs
+++ utils.cs
@@ -97,283 +97,294 @@ namespace wowCVAHK
             return dpiX / 96f;
         }
 
-    }
-    internal class IniFile
-    {
-        private Dictionary<string, string> data = new Dictionary<string, string>();
-
-        public IniFile(string filePath)
+        internal static Size DESKTOP
         {
-            // 读取或创建文件内容
-            if (File.Exists(filePath))
+            get
             {
-                foreach (var line in File.ReadAllLines(filePath))
-                {
-                    if (line.Contains("="))
-                    {
-                        var parts = line.Split('=');
-                        data[parts[0].Trim()] = parts[1].Trim();
-                    }
-                }
+                IntPtr hdc = BaseAPI.GetDC(IntPtr.Zero);
+                Size size = new Size();
+                size.Height = BaseAPI.GetDeviceCaps(hdc, 117);
+                size.Width = BaseAPI.GetDeviceCaps(hdc, 118);
+                BaseAPI.ReleaseDC(IntPtr.Zero, hdc);
+                return size;
             }
         }
 
-        internal string Get(string key)
+        //获取屏幕显示大小
+        Rectangle tScreenRect = Screen.PrimaryScreen.Bounds;
+
+        internal static Bitmap GetScreenImage()
         {
-            return data.ContainsKey(key) ? data[key] : null;
-        }
+            //获取屏幕显示大小
+            Rectangle viewRect = Screen.PrimaryScreen.Bounds;
+            Size phisicalRect = DESKTOP;
+            Bitmap tSrcBmp = new Bitmap(phisicalRect.Width, phisicalRect.Height); // 用于屏幕原始图片保存
+            Graphics gp = Graphics.FromImage(tSrcBmp);
+            gp.CopyFromScreen(0, 0, 0, 0, phisicalRect);
 
-        internal Dictionary<string, string> GetConfMappings()
-        {
-            Dictionary<string, string> colorMappings = new Dictionary<string, string>();
+            Console.WriteLine($"viewRect.Size = {viewRect.Size}, phisicalRect = {phisicalRect}");
 
-            foreach (var entry in data)
+            if (viewRect.Size != phisicalRect)
             {
-                colorMappings[entry.Key] = entry.Value;
-            }
-
-            return colorMappings;
-        }
-
-        internal static void saveIni(string key, string value)
-        {
-            if (ValidateRGB(key) && !string.IsNullOrWhiteSpace(value))
-            {
-                IniFile ini = new IniFile(Constant.INI_FILE_PATH);
-                // 读取所有映射
-                Dictionary<string, string> colorMappings = ini.GetConfMappings();
-                // 保存ini信息
-                // 将色块和按键绑定                
-                //******************** 校验 ********************
-                if (colorMappings.ContainsKey(key))
-                {
-                    colorMappings[key] = value.ToUpper();
-                }
-                else if (colorMappings.ContainsValue(value))
-                {
-                    string existingKey = FindKeyByValue(colorMappings, value);
-                    if (existingKey != null)
-                    {
-                        colorMappings.Remove(existingKey);
-                    }
-                    colorMappings[key] = value.ToUpper();
-                }
-                else
-                {
-                    colorMappings[key] = value.ToUpper();
-                }
-                WriteIniFile(colorMappings); // 保存到 INI 文件
-
-            }
-            else if (key == "windowHandle" || key == "coordinate")
-            {
-                IniFile ini = new IniFile(Constant.INI_FILE_PATH);
-                // 读取所有映射
-                Dictionary<string, string> colorMappings = ini.GetConfMappings();
-                if (colorMappings.ContainsKey(key))
-                {
-                    colorMappings[key] = value.ToUpper();
-                }
-                else
-                {
-                    colorMappings.Add(key, value.ToUpper());
-                }
-
-                WriteIniFile(colorMappings); // 保存到 INI 文件
+                //当两者不相同时进行缩放
+                Bitmap tSrcBmp2 = new Bitmap(viewRect.Width, viewRect.Height);
+                Graphics gp2 = Graphics.FromImage(tSrcBmp2);
+                gp2.DrawImage(tSrcBmp, new Rectangle(0, 0, viewRect.Width, viewRect.Height), 0, 0, phisicalRect.Width, phisicalRect.Height, GraphicsUnit.Pixel);
+                return tSrcBmp2 as Bitmap;
             }
             else
             {
-                MessageBox.Show("Invalid RGB or Key format", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return tSrcBmp;
             }
-        }
 
-        internal static string FindKeyByValue(Dictionary<string, string> iniData, string value)
+        }
+    }
+        internal class IniFile
         {
-            // 查重
-            foreach (var entry in iniData)
+            private Dictionary<string, string> data = new Dictionary<string, string>();
+
+            public IniFile(string filePath)
             {
-                if (entry.Value == value)
+                // 读取或创建文件内容
+                if (File.Exists(filePath))
                 {
-                    return entry.Key;
+                    foreach (var line in File.ReadAllLines(filePath))
+                    {
+                        if (line.Contains("="))
+                        {
+                            var parts = line.Split('=');
+                            data[parts[0].Trim()] = parts[1].Trim();
+                        }
+                    }
                 }
             }
-            return null;
-        }
 
-        static void WriteIniFile(Dictionary<string, string> iniData)
-        {
-            //写入ini
-            using (StreamWriter writer = new StreamWriter(Constant.INI_FILE_PATH))
+            internal string Get(string key)
             {
+                return data.ContainsKey(key) ? data[key] : null;
+            }
+
+            internal Dictionary<string, string> GetConfMappings()
+            {
+                Dictionary<string, string> colorMappings = new Dictionary<string, string>();
+
+                foreach (var entry in data)
+                {
+                    colorMappings[entry.Key] = entry.Value;
+                }
+
+                return colorMappings;
+            }
+
+            internal static void saveIni(string key, string value)
+            {
+                if (ValidateRGB(key) && !string.IsNullOrWhiteSpace(value))
+                {
+                    IniFile ini = new IniFile(Constant.INI_FILE_PATH);
+                    // 读取所有映射
+                    Dictionary<string, string> colorMappings = ini.GetConfMappings();
+                    // 保存ini信息
+                    // 将色块和按键绑定                
+                    //******************** 校验 ********************
+                    if (colorMappings.ContainsKey(key))
+                    {
+                        colorMappings[key] = value.ToUpper();
+                    }
+                    else if (colorMappings.ContainsValue(value))
+                    {
+                        string existingKey = FindKeyByValue(colorMappings, value);
+                        if (existingKey != null)
+                        {
+                            colorMappings.Remove(existingKey);
+                        }
+                        colorMappings[key] = value.ToUpper();
+                    }
+                    else
+                    {
+                        colorMappings[key] = value.ToUpper();
+                    }
+                    WriteIniFile(colorMappings); // 保存到 INI 文件
+
+                }
+                else if (key == "windowHandle" || key == "coordinate")
+                {
+                    IniFile ini = new IniFile(Constant.INI_FILE_PATH);
+                    // 读取所有映射
+                    Dictionary<string, string> colorMappings = ini.GetConfMappings();
+                    if (colorMappings.ContainsKey(key))
+                    {
+                        colorMappings[key] = value.ToUpper();
+                    }
+                    else
+                    {
+                        colorMappings.Add(key, value.ToUpper());
+                    }
+
+                    WriteIniFile(colorMappings); // 保存到 INI 文件
+                }
+                else
+                {
+                    MessageBox.Show("Invalid RGB or Key format", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+            internal static string FindKeyByValue(Dictionary<string, string> iniData, string value)
+            {
+                // 查重
                 foreach (var entry in iniData)
                 {
-                    writer.WriteLine($"{entry.Key} = {entry.Value}");
+                    if (entry.Value == value)
+                    {
+                        return entry.Key;
+                    }
                 }
+                return null;
             }
-        }
 
-        internal static bool ValidateRGB(string rgb)
-        {
-            //验证RGB的格式是否正确
-            string[] parts = rgb.Split(',');
-
-            if (parts.Length != 3) return false;
-
-            foreach (string part in parts)
+            static void WriteIniFile(Dictionary<string, string> iniData)
             {
-                if (!int.TryParse(part, out int value) || value < 0 || value > 255)
+                //写入ini
+                using (StreamWriter writer = new StreamWriter(Constant.INI_FILE_PATH))
                 {
-                    return false;
+                    foreach (var entry in iniData)
+                    {
+                        writer.WriteLine($"{entry.Key} = {entry.Value}");
+                    }
                 }
             }
 
-            return true;
-        }
-    }
-
-    internal class FullScreenOverlay : Form
-    {
-        private Point mousePosition;
-        private Bitmap screenCapture;
-        public FullScreenOverlay()
-        {
-            this.DoubleBuffered = true;
-            this.FormBorderStyle = FormBorderStyle.None;
-            this.WindowState = FormWindowState.Maximized;
-            this.BackColor = Color.Black;
-            this.Opacity = 0.5;  // 设置半透明效果
-            this.TopMost = true;
-
-            // 截取整个屏幕
-            screenCapture = CaptureScreen();
-            string filePath = "screenshot.jpg";
-            screenCapture.Save(filePath);
-
-            this.MouseMove += new MouseEventHandler(OnMouseMove); // 监听鼠标移动事件
-            this.MouseDoubleClick += new MouseEventHandler(OnMouseDoubleClick); // 监听双击事件
-        }
-
-        private void OnMouseMove(object sender, MouseEventArgs e)
-        {
-            mousePosition = e.Location; // 更新鼠标位置
-            this.Invalidate(); // 重新绘制窗口，显示不透明方块
-        }
-
-        // 重写OnPaint方法，绘制非透明10x10方块
-        protected override void OnPaint(PaintEventArgs e)
-        {
-            base.OnPaint(e);
-
-            int rectSize = 100;
-            Rectangle rect = new Rectangle(mousePosition.X - rectSize / 2, mousePosition.Y - rectSize / 2, rectSize, rectSize);
-
-            if (screenCapture != null)
+            internal static bool ValidateRGB(string rgb)
             {
-                // 只绘制框内部分
-                e.Graphics.DrawImage(screenCapture, rect, rect, GraphicsUnit.Pixel);
-            }
+                //验证RGB的格式是否正确
+                string[] parts = rgb.Split(',');
 
-            // 绘制绿色边框
-            using (Pen greenPen = new Pen(Color.Green, 2))
-            {
-                e.Graphics.DrawRectangle(greenPen, rect); // 画绿色边框
-            }
-        }
+                if (parts.Length != 3) return false;
 
-        private Bitmap CaptureScreen()
-        {
-            // 获取主屏幕的宽高
-            Rectangle screenBounds = Screen.PrimaryScreen.Bounds;
-
-            // 获取 DPI 缩放比例
-            float scaleFactor = Utils.GetScalingFactor();
-
-            // 根据缩放比例调整截图区域
-            int scaledWidth = (int)(screenBounds.Width * scaleFactor);
-            int scaledHeight = (int)(screenBounds.Height * scaleFactor);
-
-            // 创建一个适应缩放的位图
-            Bitmap screenshot = new Bitmap(scaledWidth, scaledHeight);
-
-            // 使用 Graphics 对象从屏幕中拷贝图像到位图
-            using (Graphics g = Graphics.FromImage(screenshot))
-            {
-                g.CopyFromScreen(Point.Empty, Point.Empty, screenBounds.Size);
-            }
-
-            return screenshot;
-
-
-
-            //Bitmap bmpScreenCapture = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
-
-            //using (Graphics g = Graphics.FromImage(bmpScreenCapture))
-            //{
-            //    g.CopyFromScreen(0, 0, 0, 0, bmpScreenCapture.Size); // 截取整个屏幕
-            //}
-
-            //return bmpScreenCapture;
-        }
-
-        // 获取鼠标双击位置和颜色
-        private void OnMouseDoubleClick(object sender, MouseEventArgs e)
-        {
-            Point clickedPosition = e.Location;
-
-            // 将屏幕坐标转换为窗口内的坐标
-            Point ClientLocation = Utils.ChangeCoordinate(clickedPosition);
-
-            IniFile.saveIni("coordinate", $"{ClientLocation.X},{ClientLocation.Y}");
-
-
-            // 获取屏幕坐标的颜色
-            Color clickedColor = GetColorAt(clickedPosition);
-
-            // 关闭全屏窗口
-            this.Close();
-
-            // 显示坐标和颜色信息到主窗口的文本框
-            ControlForm mainForm = Application.OpenForms["ControlForm"] as ControlForm;
-            if (mainForm != null)
-            {
-                mainForm.DisplayClickedColorInfo(clickedColor);
-                mainForm.DisplayClickedCoordinateInfo(ClientLocation);
-            }
-        }
-
-        // 获取屏幕某个点的颜色
-        private Color GetColorAt(Point location)
-        {
-            using (Bitmap bmp = new Bitmap(1, 1))
-            {
-                using (Graphics g = Graphics.FromImage(bmp))
+                foreach (string part in parts)
                 {
-                    g.CopyFromScreen(location, Point.Empty, new Size(1, 1));
+                    if (!int.TryParse(part, out int value) || value < 0 || value > 255)
+                    {
+                        return false;
+                    }
                 }
-                return bmp.GetPixel(0, 0);
+
+                return true;
             }
         }
+
+        internal class FullScreenOverlay : Form
+        {
+            private Point mousePosition;
+            private Bitmap screenCapture;
+            public FullScreenOverlay()
+            {
+                this.DoubleBuffered = true;
+                this.FormBorderStyle = FormBorderStyle.None;
+                this.WindowState = FormWindowState.Maximized;
+                this.BackColor = Color.Black;
+                this.Opacity = 0.5;  // 设置半透明效果
+                this.TopMost = true;
+
+                // 截取整个屏幕
+                screenCapture = CaptureScreen();
+                string filePath = "screenshot.jpg";
+                screenCapture.Save(filePath);
+
+                this.MouseMove += new MouseEventHandler(OnMouseMove); // 监听鼠标移动事件
+                this.MouseDoubleClick += new MouseEventHandler(OnMouseDoubleClick); // 监听双击事件
+            }
+
+            private void OnMouseMove(object sender, MouseEventArgs e)
+            {
+                mousePosition = e.Location; // 更新鼠标位置
+                this.Invalidate(); // 重新绘制窗口，显示不透明方块
+            }
+
+            // 重写OnPaint方法，绘制非透明10x10方块
+            protected override void OnPaint(PaintEventArgs e)
+            {
+                base.OnPaint(e);
+
+                int rectSize = 100;
+                Rectangle rect = new Rectangle(mousePosition.X - rectSize / 2, mousePosition.Y - rectSize / 2, rectSize, rectSize);
+
+                if (screenCapture != null)
+                {
+                    // 只绘制框内部分
+                    e.Graphics.DrawImage(screenCapture, rect, rect, GraphicsUnit.Pixel);
+                }
+
+                // 绘制绿色边框
+                using (Pen greenPen = new Pen(Color.Green, 2))
+                {
+                    e.Graphics.DrawRectangle(greenPen, rect); // 画绿色边框
+                }
+            }
+
+            private Bitmap CaptureScreen()
+            {
+                return screenCapture = Utils.GetScreenImage();              
+            }
+
+            // 获取鼠标双击位置和颜色
+            private void OnMouseDoubleClick(object sender, MouseEventArgs e)
+            {
+                Point clickedPosition = e.Location;
+
+                // 将屏幕坐标转换为窗口内的坐标
+                Point ClientLocation = Utils.ChangeCoordinate(clickedPosition);
+
+                IniFile.saveIni("coordinate", $"{ClientLocation.X},{ClientLocation.Y}");
+
+
+                // 获取屏幕坐标的颜色
+                Color clickedColor = GetColorAt(clickedPosition);
+
+                // 关闭全屏窗口
+                this.Close();
+
+                // 显示坐标和颜色信息到主窗口的文本框
+                ControlForm mainForm = Application.OpenForms["ControlForm"] as ControlForm;
+                if (mainForm != null)
+                {
+                    mainForm.DisplayClickedColorInfo(clickedColor);
+                    mainForm.DisplayClickedCoordinateInfo(ClientLocation);
+                }
+            }
+
+            // 获取屏幕某个点的颜色
+            private Color GetColorAt(Point location)
+            {
+                using (Bitmap bmp = new Bitmap(1, 1))
+                {
+                    using (Graphics g = Graphics.FromImage(bmp))
+                    {
+                        g.CopyFromScreen(location, Point.Empty, new Size(1, 1));
+                    }
+                    return bmp.GetPixel(0, 0);
+                }
+            }
+        }
+
+        public partial class ControlForm : Form
+        {
+            public void DisplayClickedColorInfo(Color color)
+            {
+                // 在文本框中显示颜色信息
+                txtColor.Text = $"{color.R},{color.G},{color.B}";
+            }
+
+            public void DisplayClickedWindowHandleInfo(string title)
+            {
+                // 在文本框中显示窗口名称
+                txtWindowHandle.Text = $"{title}";
+            }
+
+            public void DisplayClickedCoordinateInfo(Point position)
+            {
+                // 在文本框中显示坐标
+                txtColorCoordinate.Text = $"X: {position.X}, Y: {position.Y}";
+            }
+        }
+
     }
-
-    public partial class ControlForm : Form
-    {
-        public void DisplayClickedColorInfo(Color color)
-        {
-            // 在文本框中显示颜色信息
-            txtColor.Text = $"{color.R},{color.G},{color.B}";
-        }
-
-        public void DisplayClickedWindowHandleInfo(string title)
-        {
-            // 在文本框中显示窗口名称
-            txtWindowHandle.Text = $"{title}";
-        }
-
-        public void DisplayClickedCoordinateInfo(Point position)
-        {
-            // 在文本框中显示坐标
-            txtColorCoordinate.Text = $"X: {position.X}, Y: {position.Y}";
-        }
-    }
-
-}
